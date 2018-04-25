17e58a1b09c7d09854208aa6e1ee51aa16ced2d0
Andre Darnius <andridarnius@gmail.com>
2018-04-25 23:53:31 +0700

    update erd

--- silorusa.sql
+++ silorusa.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.12
--- http://www.phpmyadmin.net
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 25, 2018 at 05:59 PM
--- Server version: 5.6.25
--- PHP Version: 5.6.11
+-- Host: localhost
+-- Generation Time: Apr 25, 2018 at 04:52 PM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,112 +25,181 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Table structure for table `kategori`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(2) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `dokter_spesialis`
---
-
-CREATE TABLE IF NOT EXISTS `dokter_spesialis` (
-  `id_dokter` int(11) NOT NULL,
-  `spesialis` varchar(255) NOT NULL,
-  `nomor_hp` varchar(15) NOT NULL,
-  `nama_dokter` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `jenis_lokasi`
---
-
-CREATE TABLE IF NOT EXISTS `jenis_lokasi` (
+CREATE TABLE `kategori` (
   `id_jenis_lokasi` int(11) NOT NULL,
   `nama_jenis_lokasi` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_lokasi`
+--
+
+CREATE TABLE `kategori_lokasi` (
+  `id` int(11) NOT NULL,
+  `id_lokasi` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lokasi_praktek`
+-- Table structure for table `lokasi`
 --
 
-CREATE TABLE IF NOT EXISTS `lokasi_praktek` (
+CREATE TABLE `lokasi` (
   `id_lokasi_praktek` int(3) NOT NULL,
   `nama_lokasi_praktek` varchar(255) NOT NULL,
   `alamat_lokasi_praktek` varchar(255) NOT NULL,
   `jam_buka_praktek` varchar(60) NOT NULL,
-  `nomor_hp_praktek` varchar(15) NOT NULL
+  `nomor_hp_praktek` varchar(15) NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lokasi_spesialis`
+--
+
+CREATE TABLE `lokasi_spesialis` (
+  `id` int(11) NOT NULL,
+  `id_dokter` int(11) NOT NULL,
+  `id_lokasi` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+CREATE TABLE `review` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_lokasi` int(11) NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spesialis`
+--
+
+CREATE TABLE `spesialis` (
+  `id_dokter` int(11) NOT NULL,
+  `spesialis` varchar(255) NOT NULL,
+  `nomor_hp` varchar(15) NOT NULL,
+  `nama_dokter` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(2) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `level` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `level`) VALUES
+(1, 'admin', 'admin', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admin`
+-- Indexes for table `kategori`
 --
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `dokter_spesialis`
---
-ALTER TABLE `dokter_spesialis`
-  ADD PRIMARY KEY (`id_dokter`);
-
---
--- Indexes for table `jenis_lokasi`
---
-ALTER TABLE `jenis_lokasi`
+ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_jenis_lokasi`);
 
 --
--- Indexes for table `lokasi_praktek`
+-- Indexes for table `lokasi`
 --
-ALTER TABLE `lokasi_praktek`
+ALTER TABLE `lokasi`
   ADD PRIMARY KEY (`id_lokasi_praktek`);
+
+--
+-- Indexes for table `lokasi_spesialis`
+--
+ALTER TABLE `lokasi_spesialis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `spesialis`
+--
+ALTER TABLE `spesialis`
+  ADD PRIMARY KEY (`id_dokter`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT for table `kategori`
 --
-ALTER TABLE `admin`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `dokter_spesialis`
---
-ALTER TABLE `dokter_spesialis`
-  MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `jenis_lokasi`
---
-ALTER TABLE `jenis_lokasi`
+ALTER TABLE `kategori`
   MODIFY `id_jenis_lokasi` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `lokasi_praktek`
+-- AUTO_INCREMENT for table `lokasi`
 --
-ALTER TABLE `lokasi_praktek`
+ALTER TABLE `lokasi`
   MODIFY `id_lokasi_praktek` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lokasi_spesialis`
+--
+ALTER TABLE `lokasi_spesialis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `spesialis`
+--
+ALTER TABLE `spesialis`
+  MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
